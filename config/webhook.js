06915c2a59667fcb068eb3bdf44a264594a96aa8
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-02-08 03:56:20 -0800

    ft-webhook: setup webhook

--- config/webhook.js
+++ config/webhook.js
@@ -2,6 +2,5 @@ const app = require("../app");
 
 app.post("/hook", (req, res) => {
     console.log(req.body);
-    res.status(200).json('Webhook is Active');
-    res.end();
+    res.status(200).json('Webhook is Active').end();
 });
